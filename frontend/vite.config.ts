2b275599e855ec3936087dbbdca5902203195cf3
Your Name <you@email.com>
2026-02-09 14:41:55 -0500

    Add frontend Tables component to fix build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,14 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     nodePolyfills({
-      // Whether to polyfill `node:` protocol imports.
       protocolImports: true,
-      // Whether to polyfill specific globals.
       globals: {
         Buffer: true,
         global: true,
